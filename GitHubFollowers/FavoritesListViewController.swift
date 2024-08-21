721710678e8fb7871b707748da0ffcc4daac68a9
Farhana <farhana.swe@gmail.com>
2024-08-20 21:24:29 -0700

    add 2 new view controllers

---
+++ GitHubFollowers/FavoritesListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FavoritesListViewController.swift
+//  GitHubFollowers
+//
+//  Created by Farhana Mustafa on 8/20/24.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ GitHubFollowers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  GitHubFollowers
+//
+//  Created by Farhana Mustafa on 8/20/24.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
