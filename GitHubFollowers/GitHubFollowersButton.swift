9f2ac54704ed955d24d9afe712899d36478d5138
Farhana <farhana.swe@gmail.com>
2024-08-21 20:47:17 -0700

    create GitHubFollowersButton

---
+++ GitHubFollowers/GitHubFollowersButton.swift
@@ -0,0 +1,34 @@
+//
+//  GitHubFollowersButton.swift
+//  GitHubFollowers
+//
+//  Created by Farhana Mustafa on 8/21/24.
+//
+
+import UIKit
+
+class GitHubFollowersButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero) // height and width will be set during constraint configuration
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal) 
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
