03ec0b7d3b30b52aab27e7a71719cf8bed918e0b
Farhana <farhana.swe@gmail.com>
2024-08-21 21:31:24 -0700

    fix image

--- GitHubFollowers/Search/View/SearchViewController.swift
+++ GitHubFollowers/Search/View/SearchViewController.swift
@@ -16,6 +16,7 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureLogoView()
         
     }
     
@@ -31,19 +32,14 @@ class SearchViewController: UIViewController {
         guard let logoImage = UIImage(named: "gh-logo") else {
             fatalError("gh-logo should be in the bundle")
         }
-        
         logoImageView.image = logoImage
+        
         
         NSLayoutConstraint.activate([
             logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoImageView.heightAnchor.constraint(equalToConstant: 200),
-            logoImageView.heightAnchor.constraint(equalToConstant: 200),
-            logoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            logoImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            logoImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20)
-            
-            
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
         ])
         
     }
