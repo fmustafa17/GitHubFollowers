1f3c48f7ff0c5d7de7f0597a21069d25d990f1c6
Farhana <farhana.swe@gmail.com>
2024-08-23 18:27:09 -0700

    move file

---
+++ GitHubFollowers/GitHubFollowersListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GitHubFollowersListViewController.swift
+//  GitHubFollowers
+//
+//  Created by Farhana Mustafa on 8/22/24.
+//
+
+import UIKit
+
+class GitHubFollowersListViewController: UIViewController {
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.isHidden = false
+    }
+
+}
