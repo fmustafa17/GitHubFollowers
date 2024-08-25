480915255e6345febf4e95aec891d5ec709a25d4
Farhana <farhana.swe@gmail.com>
2024-08-24 17:30:21 -0700

    Add dataTaskPublisher

--- GitHubFollowers/NetworkManager.swift
+++ GitHubFollowers/NetworkManager.swift
@@ -5,29 +5,46 @@
 //  Created by Farhana Mustafa on 8/24/24.
 //
 
+import Combine
 import Foundation
+
+enum APIError: Error {
+    case invalidURL
+    case serverError
+}
 
 class NetworkManager {
     static let shared = NetworkManager()
     
+    @Published var followers: [Follower] = []
+    private var cancellables = Set<AnyCancellable>()
+
     private init() {}
     
     /// API request to get the user's followers
     /// - Parameters:
     ///   - username: User's entered username to look up
     ///   - page: for pagination
-    ///   - completionHandler: Returns the follower list if it exists, or an error string so the UI can display this message
-    func getFollowers(for username: String, page: Int, completionHandler: @escaping ([Follower]?, String?) -> Void) {
-        let endpoint: String = Constants.baseURL.rawValue + username + "/followers" + "?per_page=100" + "&page=" + String(page)
+    func getFollowers(for username: String, page: Int) throws {
+        let endpoint: String = Constants.baseURL.rawValue + "\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
-            completionHandler(nil, "Error: Invalid URL")
-            return
+             throw APIError.invalidURL
         }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            
-        }
-        
+
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: [Follower].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+                if case .failure(let error) = completion {
+                    print(error.localizedDescription)
+                }
+            },
+                receiveValue: { [weak self] followers in
+                    self?.followers = followers
+                }
+            )
+            .store(in: &cancellables)
     }
 }
